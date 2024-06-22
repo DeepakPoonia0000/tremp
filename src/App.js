3b7f6c333fab143fa966e63e66102f3c2424278a
DeepakPoonia0000 <dpoonia0000@gmail.com>
2024-06-22 19:25:19 +0530

    depak

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Announcements from './components/Announcements';
-import Home from './components/Home';
+import Fotter from './components/Fotter';
+import Hero from './components/Hero';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
 
-    <div className="App" style={{background:"black" , width:"100%"}}>
-      <Announcements/>
-      <Home />
+    <div className="App" style={{ background: "black", width: "100%" }}>
+      <Announcements />
+      <NavBar/>
+      <Hero />
+      <Fotter />
     </div>
   );
 }
